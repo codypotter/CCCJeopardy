0fbba13ca2dd64cbb6caaae5d5c542b8c065ce50
enjoipotter <enjoipotter@gmail.com>
2018-03-14 22:49:34 -0700

    Getting the grid working

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 (function(window){
-  console.log(firebase.app.name);
+  var $ = window.$;
+  var buttonGrid = $('.button-grid');
+  buttonGrid.height(window.innerHeight);
+  var button = $('<div></div>').addClass('button');
+  for (var i = 0; i < 30; i++) {
+    var butt = button.clone().appendTo(buttonGrid);
+    if (i < 5) {
+      var text = $('<h4></h4>').addClass('button-title').text("This is");
+      butt.append(text);
+      butt.addClass('title-button');
+    }
+  }
 })(window);
