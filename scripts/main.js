d6d152cc035e6c449064d68298277ec496c80cfe
enjoipotter <enjoipotter@gmail.com>
2018-03-17 19:43:21 -0700

    working on game board

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
 (function(window){
   var $ = window.$;
   var buttonGrid = $('.button-grid');
-  buttonGrid.height(window.innerHeight);
   var button = $('<div></div>').addClass('button');
+  
+  buttonGrid.height(window.innerHeight);
+
   for (var i = 0; i < 30; i++) {
     var butt = button.clone().appendTo(buttonGrid);
     if (i < 5) {
       var text = $('<h4></h4>').addClass('button-title').text("This is a test");
-      butt.append(text);
       butt.addClass('title-button');
+    } else if (i < 10){
+      var text = $('<h3></h3>').addClass('button-title').text("100");
+    } else if (i < 15){
+      var text = $('<h3></h3>').addClass('button-title').text("200");
+    } else if (i < 20){
+      var text = $('<h3></h3>').addClass('button-title').text("300");
+    } else if (i < 25){
+      var text = $('<h3></h3>').addClass('button-title').text("400");
+    } else if (i < 30){
+      var text = $('<h3></h3>').addClass('button-title').text("500");
     }
+    butt.append(text);
   }
 })(window);
