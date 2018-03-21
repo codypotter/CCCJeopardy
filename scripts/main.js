5831148fb4a6c8ca552ac2a416e04a5c68a29644
enjoipotter <enjoipotter@gmail.com>
2018-03-20 21:20:19 -0700

    worked on login menu and transition

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,10 @@
   var App = window.App;
   var QuestionBuilder = App.QuestionBuilder;
   var $ = window.$;
-  var buttonGrid = $('.button-grid');
-  var button = $('<div></div>').addClass('button');
 
-  buttonGrid.height(window.innerHeight);
-
-  for (var i = 0; i < 30; i++) {
-    var butt = button.clone().appendTo(buttonGrid);
-    if (i < 5) {
-      var text = $('<h4></h4>').addClass('button-title').text("This is a test");
-      butt.addClass('title-button');
-    } else if (i < 10){
-      var text = $('<h3></h3>').addClass('button-title').text("100");
-    } else if (i < 15){
-      var text = $('<h3></h3>').addClass('button-title').text("200");
-    } else if (i < 20){
-      var text = $('<h3></h3>').addClass('button-title').text("300");
-    } else if (i < 25){
-      var text = $('<h3></h3>').addClass('button-title').text("400");
-    } else if (i < 30){
-      var text = $('<h3></h3>').addClass('button-title').text("500");
-    }
-    butt.append(text);
-  }
+  $('.continue-button').click(function(e){
+    var loginContainer = $('.login-container')
+    loginContainer.remove();
+    $('.intro-sidebar').width('200px');
+  });
 })(window);
