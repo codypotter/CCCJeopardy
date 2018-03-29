808e0d93d9509559022b4c5840f8d9285af4c616
enjoipotter <enjoipotter@gmail.com>
2018-03-28 21:41:03 -0700

    initial commit for google login flow

--- scripts/firebaseHandler.js
+++ scripts/firebaseHandler.js
@@ -8,10 +8,33 @@
     storageBucket: "",
     messagingSenderId: "780090118711"
   };
+  var provider = new firebase.auth.GoogleAuthProvider();
 
   function FirebaseHandler() {
     firebase.initializeApp(config);
+    console.log("firebasehandler constructor called");
   };
+
+  $('.google-login-button').click(function(e) {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log(user);
+      var uh = new App.UIHandler();
+      uh.login();
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      console.log("Error code: " + errorCode);
+      console.log("Error message: " + errorMessage);
+      console.log("Error user email: " + email);
+    });
+  });
+
+
 
   App.FirebaseHandler = FirebaseHandler;
   window.App = App;
--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,10 @@
 (function(window) {
   'use strict';
-  var App = window.App;
+  var App = window.App || {};
   var QuestionBuilder = App.QuestionBuilder;
   var FirebaseHandler = new App.FirebaseHandler();
+  var UIHandler = App.UIHandler;
   var $ = window.$;
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().useDeviceLanguage();
-
-  $('.continue-button').click(function(e) {
-    var loginContainer = $('.login-container');
-    loginContainer.remove();
-    $('.intro-sidebar').width('200px');
-  });
-
-  $('.google-login-button').click(function(e) {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-      console.log(user);
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-      console.log("Error code: " + errorCode);
-      console.log("Error message: " + errorMessage);
-      console.log("Error user email: " + email);
-    });
-  });
-
-
 
   /*  Firebase Demo
   var demoRef = firebase.database().ref('demo');
