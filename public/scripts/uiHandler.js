ab57969d2f9e40c9739e8a1bd5dd75fc0601521c
Sawyer Watts <sawyer.watts@gmail.com>
2018-04-02 13:00:12 -0700

    Fill sidebar with Quiz Game and options

--- public/scripts/uiHandler.js
+++ public/scripts/uiHandler.js
@@ -16,9 +16,43 @@ reveals a lighter background where the current selected option will reside.
     var loginContainer = $('.login-container');
     loginContainer.remove();
     $('.intro-sidebar').width('200px');
+    $('.intro-sidebar').append('<h1 class="app-title">Grid Quiz</h1>');
 
-    $('.intro-sidebar').append('<h1 class="app-title">Grid Quiz</h2>');
     // TODO: add accordions for edit/create, and buttons for play quiz
+console.log("TODO: get these links to be the user's games");
+    var playGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Play a Game</button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+      </div>
+    `;
+    $('.intro-sidebar').append(playGameDropdown);
+
+    var editGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Edit a Game</button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+      </div>
+    `;
+    $('.intro-sidebar').append(editGameDropdown);
+
+    var createGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Create a Game</button>
+          <div class="dropdown-content">
+          </div>
+      </div>
+    `;
+    $('.intro-sidebar').append(createGameDropdown);
+
     // TODO: put current selected option into not sidebar part of wrapper
   };
 
