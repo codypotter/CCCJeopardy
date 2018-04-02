bed4d8cab0098aa5066058eb2985c6bad3203f8e
Sawyer Watts <sawyer.watts@gmail.com>
2018-04-01 21:00:10 -0700

    Add Qrid Quiz header and adjust CSS

--- public/scripts/uiHandler.js
+++ public/scripts/uiHandler.js
@@ -1,3 +1,8 @@
+/* ----------------------------------------------------------------------------
+	uiHandler.js will alter index.html if the user signs in. It will remove
+the log in box, push the background to the left (making a side bar), and
+reveals a lighter background where the current selected option will reside.
+-----------------------------------------------------------------------------*/
 (function(window) {
   'use strict';
   var App = window.App || {};
@@ -11,8 +16,13 @@
     var loginContainer = $('.login-container');
     loginContainer.remove();
     $('.intro-sidebar').width('200px');
+
+    $('.intro-sidebar').append('<h1 class="app-title">Grid Quiz</h2>');
+    // TODO: add accordions for edit/create, and buttons for play quiz
+    // TODO: put current selected option into not sidebar part of wrapper
   };
 
   App.UIHandler = UIHandler;
   window.App = App;
 })(window);
+
