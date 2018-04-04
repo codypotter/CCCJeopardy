85fcc3d5919d29cd79ec7aa7cf2e02061248bbb0
Sawyer Watts <sawyer.watts@gmail.com>
2018-04-03 20:24:06 -0700

    Tweek layout of functions

--- public/scripts/uiHandler.js
+++ public/scripts/uiHandler.js
@@ -5,6 +5,7 @@ reveals a lighter background where the current selected option will reside.
 -----------------------------------------------------------------------------*/
 
 // TODO: make the drop down loaders include the games already on the server
+//	make it communicate that there's nothing if there's no stuff to load
 // TODO: generalize the dropdown html into a function(?)
 // TODO: put current selected option into not sidebar part of wrapper
 
@@ -24,67 +25,63 @@ reveals a lighter background where the current selected option will reside.
     modifySidebar();
   };
 
+  function modifySidebar() {
+    $('.intro-sidebar').width('200px');
+    $('.intro-sidebar').append('<h1 class="app-title">Grid Quiz</h1>');
+
+    var playerID = firebase.auth().currentUser.uid;
+    var playerRef = firebase.database().ref("players/" + playerID);
+
+    buildPlayGameDropdown(playerRef);
+
+    buildEditGameDropdown(playerRef);
+
+    buildCreateGameDropdown(playerRef);
+  }
+
+  function buildPlayGameDropdown(playerRef) {
+    var playGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Play a Game</button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+      </div>
+    `;
+
+    $('.intro-sidebar').append(playGameDropdown);
+  }
+
+  function buildEditGameDropdown(playerRef) {
+    var editGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Edit a Game</button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+      </div>
+    `;
+
+    $('.intro-sidebar').append(editGameDropdown);
+  }
+
+  function buildCreateGameDropdown(playerRef) {
+    var createGameDropdown = `
+      <div class="dropdown">
+        <button class="dropbtn">Create a Game</button>
+          <div class="dropdown-content">
+          </div>
+      </div>
+    `;
+
+    $('.intro-sidebar').append(createGameDropdown);
+  }
+
   App.UIHandler = UIHandler;
   window.App = App;
 })(window);
-
-
-// TODO: import these from other files ----------------------------------------
-
-
-function modifySidebar() {
-  $('.intro-sidebar').width('200px');
-  $('.intro-sidebar').append('<h1 class="app-title">Grid Quiz</h1>');
-
-  var playerID = firebase.auth().currentUser.uid;
-  var playerRef = firebase.database().ref("players/" + playerID);
-
-  buildPlayGameDropdown(playerRef);
-
-  buildEditGameDropdown(playerRef);
-
-  buildCreateGameDropdown(playerRef);
-}
-
-function buildPlayGameDropdown(playerRef) {
-  var playGameDropdown = `
-    <div class="dropdown">
-      <button class="dropbtn">Play a Game</button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-    </div>
-  `;
-
-  $('.intro-sidebar').append(playGameDropdown);
-}
-
-function buildEditGameDropdown(playerRef) {
-  var editGameDropdown = `
-    <div class="dropdown">
-      <button class="dropbtn">Edit a Game</button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-    </div>
-  `;
-
-  $('.intro-sidebar').append(editGameDropdown);
-}
-
-function buildCreateGameDropdown(playerRef) {
-  var createGameDropdown = `
-    <div class="dropdown">
-      <button class="dropbtn">Create a Game</button>
-        <div class="dropdown-content">
-        </div>
-    </div>
-  `;
-
-  $('.intro-sidebar').append(createGameDropdown);
-}
 
