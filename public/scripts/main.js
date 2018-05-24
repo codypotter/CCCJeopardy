05ee416ad5c140f2d791f5e8a93f9342bdab3036
Cody Potter <enjoipotter@gmail.com>
2018-05-24 09:36:51 -0700

    tinkering

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,11 +8,13 @@
   var firebaseHandler = new FirebaseHandler();
   var uiHandler = new UIHandler();
 
-  if (isset($_GET['id']) {
+/*
+  if ($_GET['id']) {
     // user was redirected using a link, handle that.
     firebaseHandler.handleStudentRedirect($_GET['id']);
-    
+
   }
+  */
 
   window.FirebaseHandler = firebaseHandler;
   window.UIHandler = uiHandler;
